1261dbbd8843c2c992a5ef3f9d2f67bb8a645cf8
guilherme <kadeguilherme@gmail.com>
2021-08-17 16:12:41 -0300

    Modificando home

--- lib/shared/widgets/home.dart
+++ lib/shared/widgets/home.dart
@@ -1,53 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:ordenation_mark/shared/sorting/bubble.dart';
-import 'package:ordenation_mark/shared/sorting/heap.dart';
-import 'package:ordenation_mark/shared/sorting/insertion.dart';
-import 'package:ordenation_mark/shared/sorting/merge.dart';
-import 'package:ordenation_mark/shared/widgets/ordenation_view.dart';
 
 class Home extends StatelessWidget {
-  List<int> _input = [5, 4, 80, 12, 17, 2, 87, 102, 45];
-
   Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Ordenation Mark'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: SizedBox(
-            width: double.maxFinite,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                OrdenationView(
-                  items: [..._input],
-                  methodName: 'Bubble',
-                  method: BubbleSort.sort,
-                ),
-                OrdenationView(
-                  items: [..._input],
-                  methodName: 'Insertion',
-                  method: InsertionSort.sort,
-                ),
-                OrdenationView(
-                  items: [..._input],
-                  methodName: 'Merge',
-                  method: MergeSort.sort,
-                ),
-                OrdenationView(
-                  items: [..._input],
-                  methodName: 'Heap',
-                  method: HeapSort.sort,
-                ),
-              ],
-            ),
+      body: Stack(
+        children: [
+          Container(
+            height: double.maxFinite,
+            width: 80,
+            color: Colors.red,
           ),
-        ),
+        ],
       ),
     );
   }
