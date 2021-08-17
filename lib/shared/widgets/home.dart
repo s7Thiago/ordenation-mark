14f819c8801d8bd943746c8769264e78fae37e33
guilherme <kadeguilherme@gmail.com>
2021-08-17 16:24:04 -0300

    Adc barra Lateral

--- lib/shared/widgets/home.dart
+++ lib/shared/widgets/home.dart
@@ -6,12 +6,19 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff383838),
       body: Stack(
         children: [
           Container(
             height: double.maxFinite,
-            width: 80,
-            color: Colors.red,
+            width: 130,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(60),
+                bottomRight: Radius.circular(60),
+              ),
+              color: Colors.white,
+            ),
           ),
         ],
       ),
