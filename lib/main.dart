99fd0227426b3d630040cd7cc557693688cb4745
Thiago Silva <thyagosousasilva@gmail.com>
2021-08-17 16:23:05 -0300

    Reorganizando o projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ordenation_mark/shared/widgets/home.dart';
+import 'package:ordenation_mark/pages/home.dart';
 
 void main() {
   runApp(const OrdenationMarkApp());
---
+++ lib/pages/learning.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LearningPage extends StatelessWidget {
+  const LearningPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
