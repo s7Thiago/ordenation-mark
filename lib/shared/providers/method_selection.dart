4713a4379c937110f8c9e6448d525c1854bb7ba8
guilherme <kadeguilherme@gmail.com>
2021-08-20 11:38:12 -0300

    Deixando o title do  feedback_fragment interativo

--- lib/shared/providers/method_selection.dart
+++ lib/shared/providers/method_selection.dart
@@ -3,6 +3,22 @@ import 'package:ordenation_mark/pages/learning_content/learning_content.dart';
 
 class MethodSelection extends ChangeNotifier {
   OrdenationMethodEnum? _method = OrdenationMethodEnum.bubbleSort;
+  String _title = "Bubble Sort";
+
+  String get title {
+    switch (_method) {
+      case OrdenationMethodEnum.bubbleSort:
+        return "Bubble Sort";
+      case OrdenationMethodEnum.heapSort:
+        return "Heap Sort";
+      case OrdenationMethodEnum.insertionSort:
+        return "Insertion Sort";
+      case OrdenationMethodEnum.mergeSort:
+        return "Merge Sort";
+      default:
+        return "Invalido";
+    }
+  }
 
   OrdenationMethodEnum? get method => _method;
 
