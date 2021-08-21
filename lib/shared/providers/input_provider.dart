2ae1b802d98a9517f22f53bdd602846b7dbc5625
guilherme <kadeguilherme@gmail.com>
2021-08-20 22:07:57 -0300

    Criação do Map para cada metodo

--- lib/shared/providers/input_provider.dart
+++ lib/shared/providers/input_provider.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/widgets.dart';
-import 'package:ordenation_mark/shared/providers/chart_provider.dart';
+import 'package:ordenation_mark/pages/learning_content/learning_content.dart';
+import 'package:ordenation_mark/shared/sorting/sorting_controller.dart';
 
 class InputProvider extends ChangeNotifier {
   int valorVetor = 0;
   int valorMedia = 0;
+
+  final timesMap = {
+    OrdenationMethodEnum.bubbleSort: <double>[],
+    OrdenationMethodEnum.heapSort: <double>[],
+    OrdenationMethodEnum.insertionSort: <double>[],
+    OrdenationMethodEnum.mergeSort: <double>[]
+  };
+
+  executeSort(int tamanho, int qtde) {
+    for (var i = 0; i < qtde; i++) {
+      for (var j = 0; j < tamanho; j++) {}
+    }
+  }
 
   void updateVetor(int newValor) {
     valorVetor = newValor;
