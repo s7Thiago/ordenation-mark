e90f344265cff7846aa366a03c8c0df04cad88ec
guilherme <kadeguilherme@gmail.com>
2021-08-22 23:12:45 -0300

    Implementano quickSort

---
+++ lib/shared/sorting/quick_sort.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+
+class QuickSort {
+  // Dart Quick sort
+  static List<int> sort(List<int> list) {
+    int high = list.length - 1;
+    int low = 0;
+
+    return quickSort(list, low, high);
+  }
+
+  static List<int> quickSort(List<int> list, int low, int high) {
+    if (low < high) {
+      int pi = partition(list, low, high);
+
+      quickSort(list, low, pi - 1);
+      quickSort(list, pi + 1, high);
+    }
+    return list;
+  }
+
+  static int partition(List<int> list, low, high) {
+    // Base check
+    if (list.isEmpty) {
+      return 0;
+    }
+    // Take our last element as pivot and counter i one less than low
+    int pivot = list[high];
+
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+      // When j is < than pivot element we increment i and swap arr[i] and arr[j]
+      if (list[j] < pivot) {
+        i++;
+        swap(list, i, j);
+      }
+    }
+    // Swap the last element and place in front of the i'th element
+    swap(list, i + 1, high);
+    return i + 1;
+  }
+
+// Swapping using a temp variable
+  static void swap(List list, int i, int j) {
+    int temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+  }
+}
